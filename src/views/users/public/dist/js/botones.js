851136789cb4540870ddb36d2b59d40cc04d2bb7
JorgeG163 <jorge.gomez00304@gmail.com>
2023-07-12 08:12:05 -0500

    Primer comentario

--- src/views/users/public/dist/js/botones.js
+++
@@ -1,7 +0,0 @@
- // Crea el mapa
- const map = L.map('map-template').setView([9.315443327903317, -75.38846389797254], 18.14);
-
- L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 19,
-   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
- }).addTo(map);
